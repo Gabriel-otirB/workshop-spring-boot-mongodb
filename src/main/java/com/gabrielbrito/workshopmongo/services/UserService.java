28494a3484ec7a1d8c1d7c1910abf90aa9ff640a
Gabriel Brito <lordglayder@gmail.com>
2023-08-23 21:06:48 -0300

    Insert user with POST

--- src/main/java/com/gabrielbrito/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielbrito/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielbrito.workshopmongo.domain.User;
+import com.gabrielbrito.workshopmongo.dto.UserDTO;
 import com.gabrielbrito.workshopmongo.repositories.UserRepository;
 import com.gabrielbrito.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not founded"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
